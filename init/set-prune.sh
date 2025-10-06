a7c9b7a9a13687b4974833fd7cecc76398d9467d
galaxysrechris <155570083+galaxysrechris@users.noreply.github.com>
2025-10-06 14:39:45 +0100

    Avalanche docker validator (#8)
    * Runtime as user avalanche
    
    * Runtime as user avalanche
    
    * keys should be a folder not symlink, fixed prune command
    
    * fixed default
    
    * migrate env and files permissions
    
    * fixed lint
    
    * create missing folders
    
    * trailing slash
    
    * read write
    
    * fixed offline prune path
    
    * create staking folder
    
    ---------
    
    Co-authored-by: Mario Delgado <mario.delgado@galaxy.com>

--- init/set-prune.sh
+++ init/set-prune.sh
@@ -1,16 +1,35 @@
 #!/usr/bin/env sh
 set -eu
 
-if [ ! -f /var/lib/ava/chain-configs/C/config.json ]; then
-  mkdir -p /var/lib/ava/offline-pruning/
-  mkdir -p /var/lib/ava/chain-configs/C/
-  cp /root/config.json /var/lib/ava/chain-configs/C/
+if [ ! -f /home/avalanche/.avalanchego/chain-configs/C/config.json ]; then
+  mkdir -p /home/avalanche/.avalanchego/offline-pruning/
+  mkdir -p /home/avalanche/.avalanchego/chain-configs/C/
 fi
 
-cd /var/lib/ava/chain-configs/C/
-if [ -f /var/lib/ava/prune-marker ]; then
-  rm -f /var/lib/ava/prune-marker
+cp /home/avalanche/config.json /home/avalanche/.avalanchego/chain-configs/C/
+
+cd /home/avalanche/.avalanchego/chain-configs/C/
+if [ -f /home/avalanche/.avalanchego/prune-marker ]; then
+  rm -f /home/avalanche/.avalanchego/prune-marker
   jq '.["offline-pruning-enabled"] = true' config.json > config.tmp && mv config.tmp config.json
 else
   jq '.["offline-pruning-enabled"] = false' config.json > config.tmp && mv config.tmp config.json
+fi
+
+if [ -f "$HOME/.avalanchego/staking/signer.key" ]; then
+  echo "Found Signer Key in keys folder"
+else
+  echo "Signer Key NOT found in keys folder - generating new one"
+fi
+
+if [ -f "$HOME/.avalanchego/staking/staker.key" ]; then
+  echo "Found Staker Key in keys folder"
+else
+  echo "Staker Key NOT found in keys folder - generating new one"
+fi
+
+if [ -f "$HOME/.avalanchego/staking/staker.crt" ]; then
+  echo "Found Staker CRT in keys folder"
+else
+  echo "Staker CRT NOT found in keys folder - generating new one"
 fi
